2248c5a58a710e0731323e8ea1fe5c73366fbbec
Abhishek Jha <abhishek.jha001996@gmail.com>
2023-01-18 21:51:05 +0530

    daty-2

---
+++ ajioclone/src/Components/Piv.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Image, Box } from '@chakra-ui/react'
+
+
+export const Piv = () => {
+  return (
+    <div>
+        <Box boxSize='sm'>
+  <Image src='../Picture/UrbanCreator.jpg' alt='Dan Abramov' />
+</Box>
+    </div>
+  )
+}
+
+
